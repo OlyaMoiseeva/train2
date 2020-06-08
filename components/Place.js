f2aea7dbd6991a2ac98b618111ca2fe2d1638fc9
Оля Синани <olasinani@MacBook-Pro-Ola.local>
2020-06-08 23:40:53 +0300

    add babelrc

--- components/Place.js
+++ components/Place.js
@@ -1,9 +1,11 @@
 import Router from 'next/router'
+import Link from 'next/link'
 
 const handler = (to) => {
   Router.push({
     pathname: '/point',
     query: { to },
+    as: process.env.BACKEND_URL + '/point'
   })
 }
 
@@ -11,6 +13,8 @@ export default function Place(props) {
   return (
     <div>
       <span onClick={ () => handler(props.to.to) }>{props.to.title}</span>
+      <div><Link href={{ pathname: '/', query: { to: props.to.to } }} as={ process.env.BACKEND_URL + '/point'}><a>{props.to.title}</a></Link></div>
+      <div><Link href='/point' as={ process.env.BACKEND_URL + '/point'}><a>Point</a></Link></div>
     </div>
   )
 }
---
+++ out/_next/static/SltO3Qep3wXyqRKTl6csd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/point":[a]}}("static\u002Fchunks\u002Fccc82c9e0654518bf028db4adda148298c6e047d.1ed1a88c00b55d7f5e54.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
