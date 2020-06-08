4ab24641df18725ba20e8a3523209d01484f8ea8
Оля Синани <olasinani@MacBook-Pro-Ola.local>
2020-06-08 23:44:12 +0300

    Updates

---
+++ _next/static/_fNRDNhwvFwA5aBC9AFfy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/point":[a]}}("static\u002Fchunks\u002F34426ccfdf55010b22e123caa1835d0a4d4a26c7.1ed1a88c00b55d7f5e54.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
