e212ed0d92704048d6fb20ac606dc5271c353e31
Оля Синани <olasinani@MacBook-Pro-Ola.local>
2020-06-08 23:07:14 +0300

    Deploy gh-pages

---
+++ _next/static/6dkGd9KDwPrObOIk7qMxp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/point":[a]}}("static\u002Fchunks\u002F255fd73140e53b46e505de2cf57df883410e4ba9.1ed1a88c00b55d7f5e54.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
